450b1c67643b8317d0d59d9195566c5f772996b9
viveknathani <viveknathani2402@gmail.com>
2021-11-05 18:56:09 +0530

    feat: use args

--- main.go
+++ main.go
@@ -1,5 +1,20 @@
 package main
 
+import (
+	"os"
+
+	"github.com/viveknathani/makesite/converter"
+	"github.com/viveknathani/makesite/fileio"
+)
+
 func main() {
 
+	argLength := len(os.Args)
+	if argLength == 1 || argLength != 3 {
+		return
+	}
+
+	source := os.Args[1]
+	destination := os.Args[2]
+	fileio.TraverseAndRun(source, destination, converter.ToHTMLIfDoable)
 }
